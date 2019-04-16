ceb7a75f2c860ed78acc979a8dfa72104777054e
Hanna Engel <hengel@alumni.berklee.edu>
2019-04-16 22:13:50 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,16 +1,17 @@
 def second_challenge
-  epic_tragedy[:montague] =
-  {patriarch: {},
-    matriarch: {},
-    hero: {},
-    hero_friends: []
-  }
-
-    epic_tragedy[:capulet] =
-    {patriarch: {},
-    matriarch: {},
-    heroine: {},
-    heroine_friends:[]
-  }
+  epic_tragedy = {
+   :montague => {
+      :patriarch => {},
+      :matriarch => {},
+      :hero => {},
+      :hero_friends => []
+   },
+   :capulet => {
+      :patriarch => {},
+      :matriarch => {},
+      :heroine => {},
+      :heroine_friends => []
+   }
+}
 
 end
